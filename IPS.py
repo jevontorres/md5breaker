8de90886849a4a23f7be249b7329ddb74bd171e8
jevontorres <jevontor@usc.edu>
2021-04-08 03:48:25 -0400

    Update IPS.py
    added time reporting functionality

--- IPS.py
+++ IPS.py
@@ -1,10 +1,13 @@
-import mechanicalsoup
+import mechanicalsoup, time
 browser = mechanicalsoup.StatefulBrowser()
 file1 = open('hashes.txt', 'r')
-
+start = time.time()
 for line in file1:
 	testhash = line.strip()
 	testurl = f"https://md5.gromweb.com/?md5={testhash}"
 	browser.open(testurl)
 	test = browser.page.find(class_="long-content string")
 	print(test.string)
+end = time.time()
+print(end-start,"seconds")
+browser.close()
